5b23a3607f589e1864b76f625097cc2e05c7d92c
Simon Mika <simonmika@users.noreply.github.com>
2022-07-27 19:29:39 +0200

    Added caching of stubs and partitioning of namespaces. (#7)

--- DurableObject/Namespace.ts
+++ DurableObject/Namespace.ts
@@ -2,12 +2,16 @@ import * as platform from "../platform"
 import { Client } from "./Client"
 
 export class Namespace {
-	private constructor(private backend: platform.DurableObjectNamespace) {}
+	#objects: Record<string, Client | undefined> = {}
+	private constructor(private readonly backend: platform.DurableObjectNamespace, readonly prefix = "") {}
 	open(name: string): Client {
-		return new Client(this.backend.get(this.backend.idFromName(name)))
+		return (
+			this.#objects[name] ??
+			(this.#objects[name] = new Client(this.backend.get(this.backend.idFromName(this.prefix + name))))
+		)
 	}
-	load(id: string): Client {
-		return new Client(this.backend.get(this.backend.idFromString(id)))
+	partition(prefix: string): Namespace {
+		return new Namespace(this.backend, this.prefix + prefix)
 	}
 	static open(backend: platform.DurableObjectNamespace | any): Namespace | undefined {
 		return platform.DurableObjectNamespace.is(backend) ? new Namespace(backend) : undefined
