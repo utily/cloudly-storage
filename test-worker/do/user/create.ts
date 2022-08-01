ee6d085fdf3bd9cd65d3f27cc7d1fa533f783560
Hlebanja <thiagohtoliva@gmail.com>
2022-08-01 16:49:56 +0200

    Changed Item to User.

--- test-worker/do/user/create.ts
+++ test-worker/do/user/create.ts
@@ -1,6 +1,6 @@
 import * as gracely from "gracely"
 import * as http from "cloudly-http"
-import { Context } from "../../Context"
+import { Context, User } from "../../Context"
 import * as model from "../../model"
 import { router } from "../../router"
 
