b58336b3089b8c53b44326c333e066399edb5bb8
Hlebanja <thiagohtoliva@gmail.com>
2022-08-01 18:14:41 +0200

    Implemented create user.

--- test-worker/Context/Do/User.ts
+++ test-worker/Context/Do/User.ts
@@ -1,7 +1,14 @@
+import * as gracely from "gracely"
 import * as storage from "cloudly-storage"
+import * as model from "../../model"
 
 export class User {
 	private constructor(private readonly backend: storage.DurableObject.Namespace) {}
+
+	async create(user: model.User): Promise<model.User | gracely.Error> {
+		const client = this.backend.open("test")
+		return await client.post<model.User>("/user/create", user)
+	}
 
 	static open(backend?: storage.DurableObject.Namespace): User | undefined {
 		return backend ? new User(backend) : undefined
