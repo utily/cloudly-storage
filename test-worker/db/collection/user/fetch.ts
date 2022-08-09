d5b87c56f0b71f07f908a8ea0d8507d892f9d701
Marcus Hanses <marcus@intergiro.com>
2022-08-09 14:33:18 +0200

    Added tests for buffer and fixed broken functions.

--- test-worker/db/collection/user/fetch.ts
+++ test-worker/db/collection/user/fetch.ts
@@ -1,23 +1,22 @@
 import * as gracely from "gracely"
 import * as http from "cloudly-http"
 import { Context } from "../../../Context"
-import * as model from "../../../model"
 import { router } from "../../../router"
 
 export async function fetch(request: http.Request, context: Context): Promise<http.Response.Like | any> {
-	let result: model.User | gracely.Error
+	let result: gracely.Result | gracely.Error
 	const database = context.collection
-	const id = request.parameter.id
+	const id: string | undefined = request.parameter.id
 	if (!request.header.authorization)
 		result = gracely.client.unauthorized()
 	else if (!id || id.length <= 1 || id < "a" || id > "z")
-		result = gracely.client.invalidPathArgument("item/:id", "id", "string", "A valid identifier is required.")
+		result = gracely.client.invalidPathArgument("user/:id", "id", "string", "A valid identifier is required.")
 	else if (gracely.Error.is(database))
 		result = database
 	else {
 		const response = await database.users.load(id)
 		result =
-			response ??
+			gracely.success.ok(response) ??
 			gracely.client.invalidPathArgument("user/:id", "id", "string", "Unable to find user with that identifier.")
 	}
 	return result
