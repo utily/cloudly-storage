68680c2b60740035ca2f29b23f40b4be56f8329d
marcushanses <79835961+marcushanses@users.noreply.github.com>
2022-09-15 13:23:42 +0200

    Dynamic archivist (#39)
    * wip
    
    * Possible fixes for alarm timers
    
    * add default alarmTime on constructor
    
    * Made the archivist save safe amounts of documents.
    
    Archivist now stores 600 documents per trigger.
    
    * Remove bufferOnly endpoint.
    
    Co-authored-by: Hlebanja <thiagohtoliva@gmail.com>

--- Database/Buffer/Backend/Archivist.ts
+++ Database/Buffer/Backend/Archivist.ts
@@ -7,7 +7,13 @@ import { DurableObjectState, KVNamespace } from "../../../platform"
 import { Key } from "../../Key"
 import { Storage } from "./Storage"
 
+interface ToArchive {
+	complete: Record<string, string[]>
+	partial?: [string, string[]]
+}
+
 export class Archivist {
+	private archivizationLimit = 600
 	private constructor(
 		private readonly archive: Archive<Document & Record<string, any>>,
 		private readonly storage: Storage,
@@ -25,32 +31,57 @@ export class Archivist {
 	}
 	async reconcile(threshold: isoly.DateTime): Promise<((Document & Record<string, any>) | undefined)[]> {
 		const changed = Array.from((await this.state.storage.list<string>({ prefix: "changed/" })).entries())
-		const lastArchived: isoly.DateTime | undefined = await this.state.storage.get<string>("archived")
-		lastArchived && (await this.removeArchived(changed, lastArchived))
-		const staleKeys = await this.getStaleKeys(threshold, changed, lastArchived)
-		const listed: Document[] | undefined = (await this.storage.load<Document>(staleKeys)) ?? []
+		const archived: isoly.DateTime | [string, string] | undefined = await this.state.storage.get<
+			string | [string, string]
+		>("archived")
+		const lastArchived = !archived ? "" : typeof archived == "string" ? archived : Key.getLast(archived[0])
+		archived && (await this.removeArchived(changed, lastArchived))
+		const { complete, partial }: ToArchive = await this.getStaleKeys(threshold, changed, archived)
+		const listed: Document[] | undefined =
+			(await this.storage.load<Document>([...Object.values(complete).flat(), ...(partial?.[1] ?? [])])) ?? []
 		const stored = listed && listed.length > 0 ? await this.archive.store(listed, true) : []
-		await this.state.storage.put("archived", threshold)
-		return stored // TODO what to do if everything wasn't archived.
+		stored &&
+			!stored.some(e => !e) &&
+			(await this.state.storage.put("archived", partial ? [partial[0], partial[1].slice(-1)[0]] : threshold))
+		return stored
 	}
 	private async removeArchived(changed: [string, string][], lastArchived: isoly.DateTime): Promise<any> {
-		const promises = []
+		const keys = []
 		const remove = isoly.DateTime.previousSecond(lastArchived, this.configurations.remove)
 		for (const [key, value] of changed) {
 			if (Key.getLast(key) < (remove ?? "")) {
 				const docKeys = value.split("\n")
 				const idIndexKey = docKeys.map(e => "id/" + Key.getLast(e))
-				promises.push(this.storage?.remove([...docKeys, ...idIndexKey, key]))
+				keys.push(...docKeys, ...idIndexKey, key)
 			}
 		}
-		return await Promise.all(promises)
+		return await this.storage.remove(keys)
 	}
-	private async getStaleKeys(threshold: string, changes?: [string, string][], archived?: string): Promise<string[]> {
-		const result: string[] = []
+	private async getStaleKeys(
+		threshold: string,
+		changes?: [string, string][],
+		archived?: string | [string, string]
+	): Promise<ToArchive> {
+		const result: ToArchive = { complete: {} }
+		let length = 0
+		const [lastArchivedKey, lastArchived] = !archived || typeof archived == "string" ? [] : archived
 		if (changes)
 			for (const [key, value] of changes) {
-				const keyDate = Key.getAt(key, 1)
-				result.push(...((archived ?? "") <= keyDate && keyDate < threshold ? value.split("\n") : []))
+				const keyDate = Key.getLast(key)
+				if (
+					(typeof archived == "string" ? archived : Key.getLast(lastArchivedKey ?? "")) <= keyDate &&
+					keyDate < threshold
+				) {
+					const values = value.split("\n")
+					const documentKeys = lastArchived ? values.slice(values.indexOf(lastArchived) + 1) : values
+					if (length + documentKeys.length < this.archivizationLimit) {
+						result.complete[key] = documentKeys
+						length += documentKeys.length
+					} else {
+						result.partial = [key, documentKeys.slice(0, this.archivizationLimit)]
+						break
+					}
+				}
 			}
 		return result
 	}
