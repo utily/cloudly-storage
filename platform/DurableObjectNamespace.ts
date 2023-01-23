01feed25719cc9a10e3b067d0a1af81204cf322e
Lasse Löfquist <github@tvartom.com>
2023-01-23 11:53:43 +0100

    Remove comments.

--- platform/DurableObjectNamespace.ts
+++ platform/DurableObjectNamespace.ts
@@ -1,16 +1,5 @@
 import * as platform from "@cloudflare/workers-types"
 
-// import { DurableObjectId } from "./DurableObjectId"
-// import { DurableObjectNamespaceNewUniqueIdOptions } from "./DurableObjectNamespaceNewUniqueIdOptions"
-// import { DurableObjectStub } from "./DurableObjectStub"
-
-// export interface DurableObjectNamespace {
-// 	newUniqueId(options?: DurableObjectNamespaceNewUniqueIdOptions): DurableObjectId
-// 	idFromName(name: string): DurableObjectId
-// 	idFromString(id: string): DurableObjectId
-// 	get(id: DurableObjectId): DurableObjectStub
-// }
-
 export namespace DurableObjectNamespace {
 	export function is(value: platform.DurableObjectNamespace | any): value is platform.DurableObjectNamespace {
 		return (
