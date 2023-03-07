7f4df6de64060ae4e22448d180908b1ebf9055fa
Lasse Löfquist <github@tvartom.com>
2023-03-07 13:24:06 +0100

    Generics in reexport of client.

--- DurableObject/index.ts
+++ DurableObject/index.ts
@@ -1,7 +1,8 @@
+import { Error } from "../Error"
 import { Alarm } from "./Alarm"
 import { Client as ClientClass } from "./Client"
 import { Namespace } from "./Namespace"
 
-type Client = ClientClass // only export interface
+type Client<E = Error> = ClientClass<E> // only export interface
 
 export { Alarm, Client, Namespace }
