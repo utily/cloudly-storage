130e151eef75f2e14547484bacdaf8c7679e101d
NordenBear <55486503+NordenBear@users.noreply.github.com>
2023-03-16 13:13:11 +0100

    Fix for cloudflare error 1101 (#103)
    * Fix for cloudflare error 1101
    
    * Removed log
    
    * Fix
    
    ---------
    
    Co-authored-by: Björn Seifert <bjorn.seifert@pax2pay.com>

--- KeyValueStore/range.ts
+++ KeyValueStore/range.ts
@@ -1,12 +1,46 @@
 import * as cryptly from "cryptly"
+import * as platform from "@cloudflare/workers-types"
 import { Continuable } from "./Continuable"
 import { ListItem } from "./ListItem"
 import { ListOptions } from "./ListOptions"
 
 export function range(
+	data: platform.KVNamespaceListResult<unknown, string>,
+	option: ListOptions
+): platform.KVNamespaceListResult<unknown, string> {
+	let start: string, end: string
+	const result = data
+	if (option.range) {
+		start = option.range[0] ?? ""
+		end = option.range[1] ?? ""
+	} else
+		return result
+	if (end == "")
+		result.keys = result.keys.filter(i => i.name >= start)
+	else
+		result.keys = result.keys.filter(i => i.name >= start && i.name < end)
+	if (option.limit && result.keys.length > option.limit) {
+		result.keys = result.keys.slice(0, option.limit)
+		result.list_complete = false
+		if (!result.list_complete)
+			result.cursor = cryptly.Base64.encode(result.keys[option.limit - 1].name, "url")
+	}
+	const lastItem = data.keys.at(-1)
+	if (
+		!data.list_complete &&
+		((result.keys.length == 0 && data.keys.length) ||
+			(result.keys.length && lastItem && result.keys.includes(lastItem)))
+	) {
+		result.list_complete = false
+		if (!result.list_complete)
+			result.cursor = cryptly.Base64.encode(data.keys[data.keys.length - 1].name, "url")
+	}
+	return result
+}
+
+export function rangeLocal(
 	data: Continuable<ListItem<any, any>>,
-	option: ListOptions,
-	moreDataExists: boolean
+	option: ListOptions
 ): Continuable<ListItem<any, any>> {
 	let start: string, end: string
 	let result = data
@@ -24,10 +58,7 @@ export function range(
 		result.cursor = cryptly.Base64.encode(result[option.limit - 1].key, "url")
 	}
 	const lastItem = data.at(-1)
-	if (
-		moreDataExists &&
-		((result.length == 0 && data.length) || (result.length && lastItem && result.includes(lastItem)))
-	)
+	if ((result.length == 0 && data.length) || (result.length && lastItem && result.includes(lastItem)))
 		result.cursor = cryptly.Base64.encode(data[data.length - 1].key, "url")
 	return result
 }
