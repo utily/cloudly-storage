6cf84089151d6ac21018dae9de718ed5bdb0e153
Anton Kroner <anton.kroner@pax2pay.com>
2023-03-20 15:38:20 +0100

    Problems with Alarm.

--- test-worker/Context/Do/alarm/set.ts
+++ test-worker/Context/Do/alarm/set.ts
@@ -6,12 +6,20 @@ import { router } from "../router"
 export async function set(request: http.Request, context: Context): Promise<http.Response.Like | any> {
 	const alarm = context.alarm
 	const now = isoly.DateTime.now()
-	await alarm.recurring("printEveryFive", "minutes", 5)
-	await alarm.recurring("printEveryTen", "minutes", 10)
-	await alarm.recurring("printEveryFifteen", "minutes", 15)
-
-	for (let i = 1; i < 6; i++)
-		await alarm.set(`printAfter${i}`, isoly.DateTime.nextMinute(now, i))
+	//wait context.state.storage.delete("alarms|")
+	//wait context.state.storage.delete("alarms|recurring")
+	//await context.state.storage.put("alarms|", [{ time: isoly.DateTime.previousHour(now), action: "printEveryFifteen" }])
+	//await alarm.set("printEveryFifteen", isoly.DateTime.previousHour(now, 3))
+	await alarm.set("printAfter5", isoly.DateTime.nextSecond(now, 30))
+	//await alarm.recurring("printEveryFive", "minutes", 5)
+	//await alarm.recurring("printEveryTen", "minutes", 10)
+	//await alarm.recurring("printEveryFifteen", "minutes", 15)
+	//
+	//for (let i = 1; i < 6; i++)
+	//	await alarm.set(`printAfter${i}`, isoly.DateTime.nextMinute(now, i))
+	const test = await context.state.storage.list()
+	//await context.state.storage.deleteAll()
+	console.log(test)
 
 	return "success"
 }
--- test-worker/Context/Do/index.ts
+++ test-worker/Context/Do/index.ts
@@ -8,10 +8,22 @@ import "./alarm"
 export class Do {
 	private constructor(private readonly state: DurableObjectState, private readonly environment: Environment) {}
 	async fetch(request: Request): Promise<Response> {
+		console.log("asdf")
+
 		return Context.handle(request, this.environment, this.state)
 	}
 	async alarm(): Promise<void> {
 		const context = new Context(this.environment, this.state)
+		await context.alarm.register("printAfter1", context.alarmTester1.bind(context))
+		await context.alarm.register("printAfter2", context.alarmTester2.bind(context))
+		await context.alarm.register("printAfter3", context.alarmTester3.bind(context))
+		await context.alarm.register("printAfter4", context.alarmTester4.bind(context))
+		await context.alarm.register("printAfter5", context.alarmTester5.bind(context))
+		await context.alarm.register("printEveryFive", context.alarmTesterRecurring5.bind(context))
+		await context.alarm.register("printEveryTen", context.alarmTesterRecurring10.bind(context))
+		await context.alarm.register("printEveryFifteen", context.alarmTesterRecurring15.bind(context))
 		await context.alarm.handle()
+		const test = await context.state.storage.list()
+		console.log(test)
 	}
 }
