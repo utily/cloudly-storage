f68f736192f198522f1f609aa0457e0e6d40c63a
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2023-07-12 16:26:20 +0200

    Fixed alarm duplication problem. (#150)
    * Fixed alarm duplication problem.
    
    * Fixed audit.

--- test-worker/Context/Users.ts
+++ test-worker/Context/Users.ts
@@ -1,4 +1,4 @@
-import * as isoly from "isoly"
+//import * as isoly from "isoly"
 import * as storage from "cloudly-storage"
 import * as model from "../model"
 
@@ -6,23 +6,23 @@ export class Users {
 	private constructor(private readonly store: storage.KeyValueStore<model.User>) {}
 
 	async initialize(): Promise<void> {
-		await this.store.set("aaa", { level: 1, id: "aaa", groups: [], name: "aaa", created: isoly.DateTime.now() })
-		await this.store.set("aab", { level: 1, id: "aab", groups: [], name: "aab", created: isoly.DateTime.now() })
-		await this.store.set("aac", { level: 1, id: "aac", groups: [], name: "aac", created: isoly.DateTime.now() })
-		await this.store.set("aad", { level: 1, id: "aad", groups: [], name: "aad", created: isoly.DateTime.now() })
-		await this.store.set("aba", { level: 1, id: "aba", groups: [], name: "aba", created: isoly.DateTime.now() })
-		await this.store.set("abb", { level: 1, id: "abb", groups: [], name: "abb", created: isoly.DateTime.now() })
-		await this.store.set("abc", { level: 1, id: "abc", groups: [], name: "abc", created: isoly.DateTime.now() })
-		await this.store.set("abd", { level: 1, id: "abd", groups: [], name: "abd", created: isoly.DateTime.now() })
-		await this.store.set("abe", { level: 1, id: "abe", groups: [], name: "abe", created: isoly.DateTime.now() })
-		await this.store.set("aca", { level: 1, id: "aca", groups: [], name: "aca", created: isoly.DateTime.now() })
-		await this.store.set("acb", { level: 1, id: "acb", groups: [], name: "acb", created: isoly.DateTime.now() })
-		await this.store.set("acc", { level: 1, id: "acc", groups: [], name: "acc", created: isoly.DateTime.now() })
-		await this.store.set("acd", { level: 1, id: "acd", groups: [], name: "acd", created: isoly.DateTime.now() })
-		await this.store.set("ace", { level: 1, id: "ace", groups: [], name: "ace", created: isoly.DateTime.now() })
-		await this.store.set("acf", { level: 1, id: "acf", groups: [], name: "acf", created: isoly.DateTime.now() })
-		await this.store.set("acg", { level: 1, id: "acg", groups: [], name: "acg", created: isoly.DateTime.now() })
-		await this.store.set("ach", { level: 1, id: "ach", groups: [], name: "ach", created: isoly.DateTime.now() })
+		// await this.store.set("aaa", { level: 1, id: "aaa", groups: [], name: "aaa", created: isoly.DateTime.now() })
+		// await this.store.set("aab", { level: 1, id: "aab", groups: [], name: "aab", created: isoly.DateTime.now() })
+		// await this.store.set("aac", { level: 1, id: "aac", groups: [], name: "aac", created: isoly.DateTime.now() })
+		// await this.store.set("aad", { level: 1, id: "aad", groups: [], name: "aad", created: isoly.DateTime.now() })
+		// await this.store.set("aba", { level: 1, id: "aba", groups: [], name: "aba", created: isoly.DateTime.now() })
+		// await this.store.set("abb", { level: 1, id: "abb", groups: [], name: "abb", created: isoly.DateTime.now() })
+		// await this.store.set("abc", { level: 1, id: "abc", groups: [], name: "abc", created: isoly.DateTime.now() })
+		// await this.store.set("abd", { level: 1, id: "abd", groups: [], name: "abd", created: isoly.DateTime.now() })
+		// await this.store.set("abe", { level: 1, id: "abe", groups: [], name: "abe", created: isoly.DateTime.now() })
+		// await this.store.set("aca", { level: 1, id: "aca", groups: [], name: "aca", created: isoly.DateTime.now() })
+		// await this.store.set("acb", { level: 1, id: "acb", groups: [], name: "acb", created: isoly.DateTime.now() })
+		// await this.store.set("acc", { level: 1, id: "acc", groups: [], name: "acc", created: isoly.DateTime.now() })
+		// await this.store.set("acd", { level: 1, id: "acd", groups: [], name: "acd", created: isoly.DateTime.now() })
+		// await this.store.set("ace", { level: 1, id: "ace", groups: [], name: "ace", created: isoly.DateTime.now() })
+		// await this.store.set("acf", { level: 1, id: "acf", groups: [], name: "acf", created: isoly.DateTime.now() })
+		// await this.store.set("acg", { level: 1, id: "acg", groups: [], name: "acg", created: isoly.DateTime.now() })
+		// await this.store.set("ach", { level: 1, id: "ach", groups: [], name: "ach", created: isoly.DateTime.now() })
 	}
 	async list(
 		options?: storage.KeyValueStore.ListOptions
--- test-worker/do/alarm/fetch.ts
+++ test-worker/do/alarm/fetch.ts
@@ -3,14 +3,14 @@ import * as http from "cloudly-http"
 import { Context } from "../../Context"
 import { router } from "../../router"
 
-export async function alarm(request: http.Request, context: Context): Promise<http.Response.Like | any> {
+export async function get(request: http.Request, context: Context): Promise<http.Response.Like | any> {
 	let result: string | gracely.Error
 	const userClient = context.users
 
 	if (gracely.Error.is(userClient))
 		result = userClient
 	else
-		result = await userClient.alarmSet()
+		result = await userClient.alarmGet()
 	return result
 }
-router.add("POST", "/do/alarm", alarm)
+router.add("GET", "/do/alarm", get)
