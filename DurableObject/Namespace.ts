3a08118fe6b32bc67ed29524fe6368fd278ff196
marcushanses <79835961+marcushanses@users.noreply.github.com>
2024-11-27 16:15:09 +0100

    Better open functions for durable objects. (#175)

--- DurableObject/Namespace.ts
+++ DurableObject/Namespace.ts
@@ -9,11 +9,23 @@ export class Namespace<E = Error> {
 
 	open(name?: string, id?: string, options?: platform.DurableObjectNamespaceNewUniqueIdOptions): Client<E> {
 		return typeof name == "string"
-			? this.#objects[name] ??
-					(this.#objects[name] = new Client<E>(this.backend.get(this.backend.idFromName(this.partitions + name))))
+			? (this.#objects[name] ??= new Client<E>(this.backend.get(this.backend.idFromName(this.partitions + name))))
 			: typeof id == "string"
 			? new Client<E>(this.backend.get(this.backend.idFromString(id)))
 			: new Client<E>(this.backend.get(this.backend.newUniqueId(options)))
+	}
+	fromName(name: string, options?: platform.DurableObjectNamespaceGetDurableObjectOptions): Client<E> {
+		return (this.#objects[name] ??= new Client<E>(
+			this.backend.get(this.backend.idFromName(this.partitions + name), options)
+		))
+	}
+	fromId(
+		id?: string,
+		options?: platform.DurableObjectNamespaceNewUniqueIdOptions & platform.DurableObjectNamespaceGetDurableObjectOptions
+	): Client<E> {
+		return typeof id == "string"
+			? new Client<E>(this.backend.get(this.backend.idFromString(id), options))
+			: new Client<E>(this.backend.get(this.backend.newUniqueId(options), options))
 	}
 	partition(...partition: string[]): Namespace<E> {
 		return new Namespace<E>(this.backend, this.partitions + partition.join("/") + "/")
