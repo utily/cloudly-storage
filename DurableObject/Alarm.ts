8b45a415f4e4e417a9a836d45b056dea50742ae2
Anton Kroner <anton.kroner@pax2pay.com>
2025-02-03 10:57:06 +0100

    Added types to DO alarms.

--- DurableObject/Alarm.ts
+++ DurableObject/Alarm.ts
@@ -2,22 +2,15 @@ import { isoly } from "isoly"
 import * as platform from "@cloudflare/workers-types"
 
 export class Alarm {
-	private alarms?: { time: isoly.DateTime; action: string }[]
-	private recurringAlarms?: {
-		next: isoly.DateTime
-		unit: "years" | "months" | "days" | "hours" | "minutes" | "seconds" | "milliseconds"
-		interval: number
-		action: string
-	}[]
-
+	private alarms?: Alarm.Once[]
+	private recurringAlarms?: Alarm.Recurring[]
 	private actions: Record<string, (() => Promise<void>) | undefined> = {}
-
 	constructor(private readonly storage: platform.DurableObjectStorage) {}
 
-	async set(action: string, trigger: isoly.DateTime | number): Promise<{ time: isoly.DateTime; action: string }[]> {
+	async set(action: string, trigger: isoly.DateTime | number): Promise<Alarm.Once[]> {
 		trigger = typeof trigger == "number" ? isoly.DateTime.create(trigger, "milliseconds") : trigger
 		const next = (await this.storage.getAlarm()) ?? undefined
-		this.alarms ??= (await this.storage.get<{ time: isoly.DateTime; action: string }[]>("alarms|")) ?? []
+		this.alarms ??= (await this.storage.get<Alarm.Once[]>("alarms|")) ?? []
 		this.alarms.push({ time: trigger, action: action })
 		await this.storage.put("alarms|", this.alarms)
 		if (!(next && isoly.DateTime.create(next, "milliseconds") < trigger))
@@ -29,25 +22,10 @@ export class Alarm {
 		unit: "years" | "months" | "days" | "hours" | "minutes" | "seconds" | "milliseconds",
 		interval: number,
 		start: isoly.DateTime | number = isoly.DateTime.now()
-	): Promise<
-		{
-			next: isoly.DateTime
-			unit: "years" | "months" | "days" | "hours" | "minutes" | "seconds" | "milliseconds"
-			interval: number
-			action: string
-		}[]
-	> {
+	): Promise<Alarm.Recurring[]> {
 		start = typeof start == "number" ? isoly.DateTime.create(start, "milliseconds") : start
 		const trigger = this.#nextRecurring(start, unit, interval)
-		this.recurringAlarms =
-			(await this.storage.get<
-				{
-					next: isoly.DateTime
-					unit: "years" | "months" | "days" | "hours" | "minutes" | "seconds" | "milliseconds"
-					interval: number
-					action: string
-				}[]
-			>("alarms|recurring")) ?? []
+		this.recurringAlarms = (await this.storage.get<Alarm.Recurring[]>("alarms|recurring")) ?? []
 		this.recurringAlarms.push({ next: trigger, unit: unit, interval: interval, action: action })
 		await this.storage.put("alarms|recurring", this.recurringAlarms)
 		const next = (await this.storage.getAlarm()) ?? undefined
@@ -61,19 +39,11 @@ export class Alarm {
 	}
 	async handle(): Promise<void> {
 		const now = isoly.DateTime.now()
-		this.alarms ??= (await this.storage.get<{ time: isoly.DateTime; action: string }[]>("alarms|")) ?? []
+		this.alarms ??= (await this.storage.get<Alarm.Once[]>("alarms|")) ?? []
 
 		await Promise.all(this.alarms.filter(a => a.time <= now).map(a => this.actions[a.action]?.()))
 
-		this.recurringAlarms ??=
-			(await this.storage.get<
-				{
-					next: isoly.DateTime
-					unit: "years" | "months" | "days" | "hours" | "minutes" | "seconds" | "milliseconds"
-					interval: number
-					action: string
-				}[]
-			>("alarms|recurring")) ?? []
+		this.recurringAlarms ??= (await this.storage.get<Alarm.Recurring[]>("alarms|recurring")) ?? []
 		const occurring = this.recurringAlarms.filter(a => a.next <= now)
 		await Promise.all(occurring.map(a => this.actions[a.action]?.()))
 		occurring.forEach(o => (o.next = this.#nextRecurring(o.next, o.unit, o.interval)))
@@ -106,9 +76,9 @@ export class Alarm {
 			await this.storage.delete("alarms|")
 		}
 		if (futureOccurring.length > 0 && next.length > 0) {
-			futureOccurring[0].next > next[0].time
-				? await this.storage.setAlarm(isoly.DateTime.parse(next[0].time))
-				: await this.storage.setAlarm(isoly.DateTime.parse(futureOccurring[0].next))
+			await (futureOccurring[0].next > next[0].time
+				? this.storage.setAlarm(isoly.DateTime.parse(next[0].time))
+				: this.storage.setAlarm(isoly.DateTime.parse(futureOccurring[0].next)))
 		} else if (futureOccurring.length > 0)
 			await this.storage.setAlarm(isoly.DateTime.parse(futureOccurring[0].next))
 		else if (next.length > 0) {
@@ -152,5 +122,17 @@ export class Alarm {
 	}
 	async clear(): Promise<void> {
 		await this.storage.delete("alarms|")
+	}
+}
+export namespace Alarm {
+	export interface Once {
+		time: isoly.DateTime
+		action: string
+	}
+	export interface Recurring {
+		next: isoly.DateTime
+		unit: "years" | "months" | "days" | "hours" | "minutes" | "seconds" | "milliseconds"
+		interval: number
+		action: string
 	}
 }
